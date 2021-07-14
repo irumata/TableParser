6be94cb17673fbe5e2e457af7de93639c378a2ed
ScarletDragon3101 <ayan1bos@gmail.com>
2021-07-15 00:55:04 +0600

    the first way to clear the date. - Was added dictionary with possible errors ______________________________________________________ pros of this method is: easy to add new error symbols. cons: Won't work as fast due to the use of a loop

--- TestTableParser.py
+++ TestTableParser.py
@@ -1,8 +1,33 @@
 import TableParser
 import pandas as pd
+
+
 class TestTableParser(TableParser.TableParserItem):
-    def __init__(self):
-        pass
-    def parse(self, series: pd.Series):
-        return (pd.to_datetime(series.str.replace("O","0"), errors = "ignore"), [])
-        
+	def __init__(self):
+		pass
+
+	def parse(self, series: pd.Series):
+		mistake = {
+			'o': '0',
+			'l': '1',
+			'b': '6',
+			'g': '9',
+			'q': '9',
+			't': '7',
+			'v': '5',
+			'G': '6',
+			'F': '7',
+			'Z': '2',
+			'Q': '2',
+			'B': '8',
+			'O': '0',
+			'D': '0',
+			'E': '3',
+			'A': '4',
+			'S': '5',
+			r'[ -/]|[:-@]|[\[-_]|[{-~]': '-',
+		}
+		for key, val in mistake.items():
+			series = series.str.replace(key, val, regex=True)
+		# print(series)
+		return (pd.to_datetime(series, errors="ignore"), [])
