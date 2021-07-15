1c02d2de6146001096c93dd8b9c50d764651832d
irumata <irumata@gmail.com>
2021-07-15 14:11:11 +0300

    add excel fixer

--- .ipynb_checkpoints/SimpleTableParser-checkpoint.py
+++
@@ -1,8 +0,0 @@
-import TableParser
-import pandas as pd
-class SimpleTableParser(TableParser.TableParserItem):
-    def __init__(self):
-        pass
-    def parse(self, series: pd.Series):
-        return (pd.to_datetime(series, errors = "ignore"), [])
-        
---
+++ excel_openner_fixer.py
@@ -0,0 +1,18 @@
+import tempfile, zipfile
+def fix_xlsx(in_file):
+    tmpfd, tmp = tempfile.mkstemp(dir=os.path.dirname(in_file))
+    os.close(tmpfd)
+    filename = '[Content_Types].xml'
+    data = ''
+    with zipfile.ZipFile(in_file, 'r') as zin:
+        with zipfile.ZipFile(tmp, 'w') as zout:
+            for item in zin.infolist():
+                if item.filename != filename:
+                    zout.writestr(item, zin.read(item.filename))
+                else:
+                    data = zin.read(filename).decode()
+    os.remove(in_file)
+    os.rename(tmp, in_file)
+    data = data.replace('/xl/sharedStrings.xml', '/xl/SharedStrings.xml')
+    with zipfile.ZipFile(in_file, mode='a', compression=zipfile.ZIP_DEFLATED) as zf:
+        zf.writestr(filename, data)
